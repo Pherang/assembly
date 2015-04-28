c62838dfd3013c2388afb676665687ea5494369a
Lablo <topher.hoang@gmail.com>
2015-04-28 11:54:50 -0400

    Cleanup of files

--- maxpointprog/asm_link_maximum.sh
+++ maxpointprog/asm_link_maximum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Remove the binary before attempting assemble and link it again.
-rm maximum
+rm ./build/maximum
 rm maximum.o
-as maximum.s -o maximum.o &&
-ld maximum.o -o maximum
+as --32 maximum.s -o maximum.o &&
+ld -m elf_i386 maximum.o -o ./build/maximum
